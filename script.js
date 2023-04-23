897634484f7c42e1295f3802aeefc7ace33deabc
klm923 <klm923@gmail.com>
2023-04-23 19:58:04 +0900

    24th js追加

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+function actRegist(event) {
+    event.preventDefault(); // フォームの送信をキャンセル
+    
+    const form = document.getElementById('myFormEntry');
+    const formData = new FormData(form);
+
+    console.log('first_name: ' + formData.get('first_name'));
+    
+    document.body.style.cursor = 'wait'; // カーソルを砂時計に変える
+    const btnRegist = document.getElementById('btnRegist');
+    btnRegist.disabled = true;
+
+    fetch('https://script.google.com/macros/s/AKfycbxBByhN2E2sePrNiKjrxDVOuFNbNlSqlhm6yBh-NBhL4y2DWYhVNzJSjszz87G8lJ7p/exec', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+    //   document.getElementById('detail_id').value = data.retID;
+      alert(`ID=${data.retID}で登録しました！`);
+      btnRegist.disabled = false;
+      document.body.style.cursor = 'auto'; // カーソルを元に戻す
+    });
+  }
